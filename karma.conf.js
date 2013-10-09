b0745728b5eec5e712aa6268cf3db2831694aeff
Rudolf Olah <rolah@outsideiq.com>
2013-10-09 16:01:03 -0400

    Updated Karma configuration file

--- karma.conf.js
+++ karma.conf.js
@@ -8,10 +8,19 @@ module.exports = function (config) {
       'Firefox'
     ],
     'files': [
+      // Angular files
       'lib/angular-1.0.8/angular.js',
-      'lib/angular-1.0.8/angular-*.js',
+      'lib/angular-1.0.8/angular-cookies.js',
+      'lib/angular-1.0.8/angular-loader.js',
+      'lib/angular-1.0.8/angular-resource.js',
+      'lib/angular-1.0.8/angular-santitize.js',
       'lib/angular-1.0.8/angular-mocks.js',
-      'js/**/*.js',
+    
+      // Hello World source code
+      'js/helloworld/app.js',
+      'js/helloworld/controllers.js',
+    
+      // Hello World test files
       'tests/*.js'
     ]
   });
